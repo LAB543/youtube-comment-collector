8b8f2cce31bd8b0b1ef9020be1cfae166106b215
Hwan Oh <hwangoon@gmail.com>
2018-08-19 16:55:21 +0900

    url making

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var data = {};
+
+  if(req.query.id) {
+    // call api
+    console.log(req.query.videoId);
+
+    var url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies"
+      + "&videoId="+req.query.videoId
+      + "&key="+process.env.YOUTUBE_API_KEY;
+
+  }
+
+  res.render('index',
+    {
+      title: 'Youtube Comment Collector',
+      data: data
+    });
 });
 
 module.exports = router;
