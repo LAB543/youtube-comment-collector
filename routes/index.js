1d6dc30e5a4a88e3b6a5383e5795abd68b394566
Hwan Oh <hwangoon@gmail.com>
2018-08-21 10:46:19 +0900

    iii

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 require('dotenv').config();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   var data = {};
 
-  if(req.query.id) {
+  if(req.query.videoId) {
     // call api
     console.log(req.query.videoId);
 
@@ -15,13 +16,22 @@ router.get('/', function(req, res, next) {
       + "&videoId="+req.query.videoId
       + "&key="+process.env.YOUTUBE_API_KEY;
 
+    request(url, function (err, res, body) {
+      if (!err && res.statusCode == 200) {
+        console.log(body) // Print the google web page.
+
+        res.render('index', {
+          title: 'Youtube Comment Collector',
+          data: body
+        });
+
+      }
+    });
+
+    return;
   }
 
-  res.render('index',
-    {
-      title: 'Youtube Comment Collector',
-      data: data
-    });
+  res.render('index', { title: 'Youtube Comment Collector' });
 });
 
 module.exports = router;
